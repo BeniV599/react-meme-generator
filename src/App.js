a45dc844a306960d9379f011aa3f3bdcb78aec9d
Benedikt <beniv599@gmail.com>
2023-05-05 16:49:32 +0200

    Added adjustments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import './App.css';
-import logo from './logo.svg';
+import './styles.css';
+import { useState } from 'react';
 
 export default function App() {
+  const [myApp, setMyApp] = useState('text');
+
+  const handleChange = (event) => {
+    setMyApp(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Me dead inside, y'all! ;D</h1>
+      <img
+        data-test-id="meme-image"
+        src="https://api.memegen.link/images/cryingfloor.png"
+        width="666px"
+        alt=""
+      />
+      <h1>It's okay tho. I love the struggle! :P</h1>
+
+      <form>
+        <select value={myApp} onChange={handleChange}>
+          <option value="Alive">Alive</option>
+          <option value="Asleep">Asleep</option>
+          <option value="Gone Deep">Gone Deep</option>
+        </select>
+      </form>
+      <br />
+      <br />
+      <form>
+        <input value="text" name="Meme" />
+      </form>
+      <br />
+      <br />
+      <form>
+        <button>
+          <span>
+            <b>Press me button, right here! :3</b>
+          </span>
+        </button>
+      </form>
+    </>
   );
 }
